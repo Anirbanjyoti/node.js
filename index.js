0abe98545460bb4970f7ab7c30e044509f8b144f
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-25 21:04:19 +0530

    Node js URL Module

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 let http = require('http');
+const URL = require('url')
+
 let server = http.createServer((req, res)=>{
-    if(req.url=="/"){
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.write('<h1>This is Home</h1>')
-        res.end();
-    }
-    else if(req.url=="/about"){
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.write('<h1>This is about</h1>')
-        res.end();
-    }
-    else if(req.url=="/contact"){
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.write('<h1>This is contact</h1>')
-        res.end();
-    }
+    // if(req.url=="/"){
+    //     res.writeHead(200, {'Content-Type':'text/html'})
+    //     res.write('<h1>This is Home</h1>')
+    //     res.end();
+    // }
+    // else if(req.url=="/about"){
+    //     res.writeHead(200, {'Content-Type':'text/html'})
+    //     res.write('<h1>This is about</h1>')
+    //     res.end();
+    // }
+    // else if(req.url=="/contact"){
+    //     res.writeHead(200, {'Content-Type':'text/html'})
+    //     res.write('<h1>This is contact</h1>')
+    //     res.end();
+    // }
+    // Ulr Module
+    const myUrl = "https://anirban-portfolio.netlify.app/skills/?year=2020";
+    const myUrlObject = URL.parse(myUrl, true);
+
+    const myHostName =myUrlObject.host;
+    const myPathName =myUrlObject.pathname;
+    const mySearchName =myUrlObject.search;
+
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.write(mySearchName);
+    res.end();
+
 })
 server.listen(5000);
 console.log(`Node Server is Running`);
