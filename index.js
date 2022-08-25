3499c04574102efdcf40548a0bc9e53737a503d7
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-25 11:07:26 +0530

    Req, res

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let http = require('http');
 let server = http.createServer((req, res)=>{
-    res.end('Hello Node Server')
+    if(req.url=="/"){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.write('<h1>This is Home</h1>')
+        res.end();
+    }
+    else if(req.url=="/about"){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.write('<h1>This is about</h1>')
+        res.end();
+    }
+    else if(req.url=="/contact"){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.write('<h1>This is contact</h1>')
+        res.end();
+    }
 })
 server.listen(5000);
 console.log(`Node Server is Running`);
