54876ed2d87cf7c420c0e76240dabd77995173e8
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-26 18:27:11 +0530

    Sync way readFile

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ let http = require("http");
 
 let server = http.createServer((req, res) => {
   // ASync Read FIle method
+  //   if (req.url == "/") {
+  //     fs.readFile("home.html", function (error, data) {
+  //       res.writeHead(200, { "Content-Type": "text/html" });
+  //       res.write(data);
+  //       res.end();
+  //     });
+  //   }
+  // Sync Read FIle method
   if (req.url == "/") {
-    fs.readFile("home.html", function (error, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    let myData = fs.readFileSync("home.html");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(myData);
+    res.end();
   }
 });
 server.listen(5050);
