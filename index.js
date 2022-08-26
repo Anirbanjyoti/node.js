6c8ec82c638c7bb78fc901253ff4caeffc9a8ed1
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-26 18:33:47 +0530

    Async way File Write

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ let fs = require("fs");
 let http = require("http");
 
 let server = http.createServer((req, res) => {
-  // ASync Read FIle method
-  //   if (req.url == "/") {
-  //     fs.readFile("home.html", function (error, data) {
-  //       res.writeHead(200, { "Content-Type": "text/html" });
-  //       res.write(data);
-  //       res.end();
-  //     });
-  //   }
-  // Sync Read FIle method
+  // ASync Write FIle method===================
+  //===============================================
   if (req.url == "/") {
-    let myData = fs.readFileSync("home.html");
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(myData);
-    res.end();
+    fs.writeFile("demo.txt", "Hello World", function (error) {
+      if (error) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Fail");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Success");
+        res.end();
+      }
+    });
   }
 });
 server.listen(5050);
