906ee734f449c273c5b3161aa3c854f982d2e5f0
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-26 18:22:04 +0530

    readFile in async way

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-let http = require('http');
-const URL = require('url')
+let fs = require("fs");
+let http = require("http");
 
-let server = http.createServer((req, res)=>{
-    // if(req.url=="/"){
-    //     res.writeHead(200, {'Content-Type':'text/html'})
-    //     res.write('<h1>This is Home</h1>')
-    //     res.end();
-    // }
-    // else if(req.url=="/about"){
-    //     res.writeHead(200, {'Content-Type':'text/html'})
-    //     res.write('<h1>This is about</h1>')
-    //     res.end();
-    // }
-    // else if(req.url=="/contact"){
-    //     res.writeHead(200, {'Content-Type':'text/html'})
-    //     res.write('<h1>This is contact</h1>')
-    //     res.end();
-    // }
-    // Ulr Module
-    const myUrl = "https://anirban-portfolio.netlify.app/skills/?year=2020";
-    const myUrlObject = URL.parse(myUrl, true);
-
-    const myHostName =myUrlObject.host;
-    const myPathName =myUrlObject.pathname;
-    const mySearchName =myUrlObject.search;
-
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.write(mySearchName);
-    res.end();
-
-})
-server.listen(5000);
+let server = http.createServer((req, res) => {
+  // ASync Read FIle method
+  if (req.url == "/") {
+    fs.readFile("home.html", function (error, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
+server.listen(5050);
 console.log(`Node Server is Running`);
-
