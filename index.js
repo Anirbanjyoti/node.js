79d388b576c698f1d059fdc9842438607b6f7732
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-26 19:15:24 +0530

    rename FIle ASync method

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ let fs = require("fs");
 let http = require("http");
 
 let server = http.createServer((req, res) => {
-  //  Write FIle Sync method===================
+  //  rename FIle ASync method===================
   //===============================================
   if (req.url == "/") {
-  let error =  fs.writeFileSync("syncDemo.txt", "Hello World write file in Sync way");
-  {
-    if (error) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("File Write Fail");
-      res.end();
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("File Write Success");
-      res.end();
-    }
-  }
-
+    fs.rename("demo.txt", "asyncDemo.txt", function (error) {
+      if (error) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Fail");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Success");
+        res.end();
+      }
+    });
   }
 });
 server.listen(5050);
